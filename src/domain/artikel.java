bd49a5e0160d4d38c1ea4d52a642d2de6e5b51f2
Angelo Lemmens <angelo.lemmens@student.ucll.be>
2020-02-18 15:06:27 +0100

    Initial commit

---
+++ src/domain/artikel.java
@@ -0,0 +1,7 @@
+package domain;
+
+public class artikel {
+    private String titel;
+    private double prijs;
+    private int voorraad;
+}
---
+++ src/domain/weekblad.java
@@ -0,0 +1,5 @@
+package domain;
+
+public class weekblad extends artikel {
+    private int weekVanUitgiffte;
+}
